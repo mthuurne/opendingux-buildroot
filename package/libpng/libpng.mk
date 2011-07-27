07afa83bd3276da226aa1799944d0708be421acc
Maarten ter Huurne <maarten@treewalker.org>
2011-07-27 11:34:35 +0200

    libpng: Go back to 1.2 series.
    This is for binary compatibility with apps compiled with booboo's toolchain.

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -3,14 +3,16 @@
 # libpng (Portable Network Graphic library)
 #
 #############################################################
-LIBPNG_VERSION = 1.4.4
-LIBPNG_SERIES = 14
+LIBPNG_VERSION = 1.2.46
+LIBPNG_SERIES = 12
 LIBPNG_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libpng
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.bz2
 LIBPNG_LIBTOOL_PATCH = NO
 LIBPNG_INSTALL_STAGING = YES
+LIBPNG_CONF_OPT = --without-libpng-compat
 LIBPNG_DEPENDENCIES = host-pkg-config zlib
 
+HOST_LIBPNG_CONF_OPT = --without-libpng-compat
 HOST_LIBPNG_DEPENDENCIES = host-pkg-config host-zlib
 
 define LIBPNG_STAGING_LIBPNG12_CONFIG_FIXUP
