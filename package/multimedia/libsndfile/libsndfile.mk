444b6bce71a6656e7fbd067344b7790258bd4e44
Maarten ter Huurne <maarten@treewalker.org>
2011-07-27 03:36:21 +0200

    libsndfile: bump to version 1.0.25
    There have been a lot of bug fixes, including a few that impact security.

--- package/multimedia/libsndfile/libsndfile.mk
+++ package/multimedia/libsndfile/libsndfile.mk
@@ -3,8 +3,7 @@
 # libsndfile
 #
 #############################################################
-
-LIBSNDFILE_VERSION = 1.0.23
+LIBSNDFILE_VERSION = 1.0.25
 LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
 LIBSNDFILE_INSTALL_STAGING = YES
 
