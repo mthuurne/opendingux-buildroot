c2cf95f65c31a715e39d711662293d4760757c31
Ayla <paul@crapouillou.net>
2011-08-31 12:45:13 +0200

    package/efl: added a package for the Evas library.

---
+++ package/efl/evas/evas.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# evas -- EFL EVAS library
+#
+################################################################################
+
+EVAS_VERSION = 1.2.0
+EVAS_SOURCE = evas-$(EVAS_VERSION).tar.bz2
+EVAS_SITE = http://download.enlightenment.org/releases/
+#EVAS_AUTORECONF = NO
+EVAS_INSTALL_STAGING = YES
+EVAS_INSTALL_TARGET = YES
+EVAS_DEPENDENCIES = host-pkg-config freetype eina
+HOST_EVAS_DEPENDENCIES = host-pkg-config host-freetype host-eina host-libpng
+
+ifeq ($(BR2_PACKAGE_EVAS_EXAMPLES),y)
+EVAS_CONF_OPT += --enable-build-examples
+endif
+
+ifeq ($(BR2_PACKAGE_EET),y)
+EVAS_DEPENDENCIES += eet
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPNG),y)
+EVAS_DEPENDENCIES += libpng
+endif
+
+ifeq ($(BR2_PACKAGE_JPEG),y)
+EVAS_DEPENDENCIES += jpeg
+endif
+
+ifeq ($(BR2_PACKAGE_TIFF),y)
+EVAS_DEPENDENCIES += tiff
+endif
+
+$(eval $(call AUTOTARGETS,package/efl,evas))
+$(eval $(call AUTOTARGETS,package/efl,evas,host))
