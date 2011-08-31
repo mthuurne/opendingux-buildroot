f1d007409ba9a22f9b0901ff58eabf7ec1892b68
Ayla <paul@crapouillou.net>
2011-08-31 12:47:39 +0200

    package/efl: added a package for the Ecore library.

---
+++ package/efl/ecore/ecore.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# ecore -- EFL ECORE library
+#
+################################################################################
+
+ECORE_VERSION = 1.2.0
+ECORE_SOURCE = ecore-$(ECORE_VERSION).tar.bz2
+ECORE_SITE = http://download.enlightenment.org/releases/
+ECORE_AUTORECONF = NO
+ECORE_INSTALL_STAGING = YES
+ECORE_INSTALL_TARGET = YES
+ECORE_DEPENDENCIES = host-pkg-config eina evas
+
+ifeq ($(BR2_PACKAGE_ECORE_ENABLE-G-MAIN-LOOP),y)
+ECORE_CONF_OPT += --enable-g-main-loop
+endif
+
+$(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
