fe68242480f112ec0f9dfefa1d5601648893d74a
Ayla <paul@crapouillou.net>
2011-08-31 12:56:33 +0200

    package/efl: added a package for the Edje library.

---
+++ package/efl/edje/edje.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# edje -- EFL EDJE library
+#
+################################################################################
+
+EDJE_VERSION = 1.2.0
+EDJE_SOURCE = edje-$(EDJE_VERSION).tar.bz2
+EDJE_SITE = http://download.enlightenment.org/releases/
+EDJE_AUTORECONF = NO
+EDJE_INSTALL_STAGING = YES
+EDJE_INSTALL_TARGET = YES
+EDJE_DEPENDENCIES = host-pkg-config fontconfig freetype lua eina evas eet embryo ecore
+EDJE_CONF_OPT += --enable-fixed-point \
+				 --disable-edje-cc \
+				 --disable-edje-decc \
+				 --disable-edje-recc \
+				 --disable-edje-player \
+				 --disable-edje-inspector \
+				 --disable-edje-external-inspector
+
+ifeq ($(BR2_PACKAGE_EDJE_AMALGAMATION),y)
+EDJE_CONF_OPT += --enable-amalgamation
+endif
+
+HOST_EDJE_DEPENDENCIES = host-pkg-config host-fontconfig host-lua host-eina host-evas host-eet host-embryo host-ecore
+
+$(eval $(call AUTOTARGETS,package/efl,edje))
+$(eval $(call AUTOTARGETS,package/efl,edje,host))
