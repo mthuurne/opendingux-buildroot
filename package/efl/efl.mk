e8ce7521eb4471f9ed3b41836d2d7bfc75e759cb
Ayla <paul@crapouillou.net>
2011-11-09 13:21:54 +0100

    package/efl/elementary: added package.

--- package/efl/efl.mk
+++ package/efl/efl.mk
@@ -5,5 +5,6 @@ include package/efl/ecore/ecore.mk
 include package/efl/eet/eet.mk
 include package/efl/embryo/embryo.mk
 include package/efl/edje/edje.mk
+include package/efl/elementary/elementary.mk
 endif
 
