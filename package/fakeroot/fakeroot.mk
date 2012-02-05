90a88eb938512b0ee534eed646fe72d9e7e08919
Maarten ter Huurne <maarten@treewalker.org>
2012-02-05 01:00:07 +0100

    fakeroot: Updated URL.
    The previous URL redirects to the main index.

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -5,7 +5,7 @@
 #############################################################
 FAKEROOT_VERSION:=1.9.5
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
-FAKEROOT_SITE:=http://snapshot.debian.net/archive/2008/04/27/debian/pool/main/f/fakeroot/
+FAKEROOT_SITE:=http://snapshot.debian.org/archive/debian/20080427T000000Z/pool/main/f/fakeroot/
 FAKEROOT_LIBTOOL_PATCH=NO
 
 define FAKEROOT_PATCH_FAKEROOT_IN
