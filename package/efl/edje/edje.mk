3a50bf32d6b45d04593bf1bbdf409d6beea70b89
Paul Cercueil <paul@crapouillou.net>
2012-05-09 14:17:37 +0200

    Edje: bump version to 1.2.0

--- package/efl/edje/edje.mk
+++ package/efl/edje/edje.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EDJE_VERSION = 1.0.0
-EDJE_SOURCE = edje-$(EDJE_VERSION).tar.gz
+EDJE_VERSION = 1.2.0
+EDJE_SOURCE = edje-$(EDJE_VERSION).tar.bz2
 EDJE_SITE = http://download.enlightenment.org/releases/
 EDJE_AUTORECONF = NO
 EDJE_INSTALL_STAGING = YES
