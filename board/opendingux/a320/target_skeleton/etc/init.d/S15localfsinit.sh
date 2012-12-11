bfdb08da63bbb6ced6313dc12a4ca44c529097ea
Maarten ter Huurne <maarten@treewalker.org>
2012-12-11 15:24:52 +0100

    fs/opendingux: Bind /boot/apps to /usr/local/home/apps
    On the GCW Zero we have a symlink for that, but our data partition is
    on vfat which lacks symlinks. A bind-type mount creates a very similar
    result though.

--- board/opendingux/a320/target_skeleton/etc/init.d/S15localfsinit.sh
+++ board/opendingux/a320/target_skeleton/etc/init.d/S15localfsinit.sh
@@ -2,10 +2,13 @@
 
 if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
 
+    mkdir -p /boot/apps
     mkdir -p /boot/local
     for i in bin etc home/apps lib sbin share; do
         mkdir -p /usr/local/$i
     done
+
+    mount /boot/apps /usr/local/home/apps -o bind
 
     if [ ! -f /etc/local/timezone ]; then
         echo 'CET-1CEST,M3.5.0,M10.5.0' > /etc/local/timezone
